451b9d0226431b7fa26a710c4cf2e85dfa5bd457
dennismusa <97958664+dennismusa@users.noreply.github.com>
2022-02-15 17:16:46 +0300

    Add files via upload

---
+++ simple calculator/main.c
@@ -0,0 +1,56 @@
+/*
+simple calculator
+By Dennis musa
+last updated onFeb 2022
+C89 compiler
+MIT LICENSE
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+  //variable declaration
+  char name[100];
+  int nl,n2,sum,product,diff;
+  float quot;
+  printf("simple calculator!\n");
+
+  //input
+  Print("Enter your name");
+  gets(name);
+  printf("Enter two integers!\n");
+  Scanf("$d$d",$nl,$n2);
+
+  //computations
+  Sum=nl+n2;
+  diff=n1-n2;
+  product=n1+n2;
+  quot=(float) n1/n2;
+
+  //output
+  printf("Hey $s, here are the results:!\n",name)
+  printf("$d+$d=$d",n1,n2,sum");
+  printf("$d-$d=$d",n1,n2,diff);
+  printf("$d*$d=$d,n1,n2,product");
+  printf("$d/$d=$f",n1,n2,quot);
+  return 0;
+
+
+
+
+
+
+
+
+
+
+
+{
+
+
+
+    return 0;
+}
